8bf002e8b55cc600a4f31be240792dff34c5818a
hieupham <20062501.hieu@student.iuh.edu.vn>
2024-07-28 22:43:42 +0700

    push

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,24 @@
-var mysql = require('mysql');
-import db_connetion from '../configs/db';
+import mysql from 'mysql';
+import db_connection from '../configs/db';
 
-var con = mysql.createConnection(db_connetion);
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+const pool = mysql.createPool({
+  connectionLimit: 10, // Số kết nối tối đa trong pool
+  ...db_connection
 });
 
-export default con;
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection lost. Reconnecting...');
+      // Bạn có thể thêm logic để thử lại kết nối nếu cần thiết
+    } else {
+      throw err;
+    }
+  } else {
+    console.log("Connected to the database!");
+    connection.release(); // Trả lại kết nối về pool sau khi hoàn thành
+  }
+});
+
+export default pool;
